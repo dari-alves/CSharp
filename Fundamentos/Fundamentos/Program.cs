b6915b729629f9bada3c172cb5e3d9120c93fba0
Dari Alves <dari.alves.pt@gmail.com>
2024-03-18 21:26:24 +0000

    fundamentals

---
+++ Fundamentos/Fundamentos/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Fundamentos;
+
+class Program
+{
+    static void Main()
+    {
+        int intNumber = 7;
+        long longNumber = 145021;
+        uint uintNumber = 12;
+
+        double doubleNumber = 3.14;
+        float floatNumber = 3.14f;
+        decimal decimalNumber = 3.14m;
+        
+        Console.WriteLine(doubleNumber);
+        Console.WriteLine(doubleNumber.GetType().Name);
+    }
+}
+
