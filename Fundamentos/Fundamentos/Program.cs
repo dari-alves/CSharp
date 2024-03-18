b7d753743ef2e78c98738c306c5205b9638d4540
Dari Alves <dari.alves.pt@gmail.com>
2024-03-18 21:44:15 +0000

    bool, string

--- Fundamentos/Fundamentos/Program.cs
+++ Fundamentos/Fundamentos/Program.cs
@@ -4,13 +4,28 @@ class Program
 {
     static void Main()
     {
+        // Integral numeric
         int intNumber = 7;
         long longNumber = 145021;
         uint uintNumber = 12;
 
+        // Floating-point
         double doubleNumber = 3.14;
         float floatNumber = 3.14f;
         decimal decimalNumber = 3.14m;
+        
+        // Boolean
+        bool active = true;
+        
+        // Char
+        char letter = 'A'; // single quotes
+        
+        // String
+        string name = "Vegeta"; // double quotes
+
+        char firstLetter = name[0];
+        
+        Console.WriteLine(firstLetter);
         
         Console.WriteLine(doubleNumber);
         Console.WriteLine(doubleNumber.GetType().Name);
