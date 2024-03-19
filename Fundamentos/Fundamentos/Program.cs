7b6a72a8713ef5fb0719b2124281d293e4ef45ac
Dari Alves <dari.alves.pt@gmail.com>
2024-03-19 22:07:47 +0000

    date, time

--- Fundamentos/Fundamentos/Program.cs
+++ Fundamentos/Fundamentos/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Globalization;
+using System.Text;
 
 namespace Fundamentos;
 
@@ -6,52 +7,66 @@ class Program
 {
     static void Main()
     {
-        // Integral numeric
-        int intNumber = 7;
-        long longNumber = 145021;
-        uint uintNumber = 12;
-
-        // Floating-point
-        double doubleNumber = 3.14;
-        float floatNumber = 3.14f;
-        decimal decimalNumber = 3.14m;
+        // // Integral numeric
+        // int intNumber = 7;
+        // long longNumber = 145021;
+        // uint uintNumber = 12;
+        //
+        // // Floating-point
+        // double doubleNumber = 3.14;
+        // float floatNumber = 3.14f;
+        // decimal decimalNumber = 3.14m;
+        //
+        // // Boolean
+        // bool active = true;
+        //
+        // // Char
+        // char letter = 'A'; // single quotes
+        //
+        // // String
+        // string name = "Vegeta"; // double quotes
+        //
+        // char firstLetter = name[0];
+        //
+        // Console.WriteLine(firstLetter);
+        //
+        // Console.WriteLine(doubleNumber);
+        // Console.WriteLine(doubleNumber.GetType().Name);
+        //
+        // string path = @"C:\test"; // "C:\\test"
+        //
+        // string firstName = "Iori";
+        // string lastName = "Yagami";
+        //
+        // string fullName = $"{firstName} {lastName}";
+        //
+        // // String Builder
+        // StringBuilder stringBuilder = new StringBuilder();
+        //
+        // stringBuilder.Append(firstName);
+        // stringBuilder.Append(lastName);
+        //
+        // string result = stringBuilder.ToString();
+        //
+        // string user = "Name: {0}. \nPassword: {1}";
+        //
+        // string userData = string.Format(user, "Vegeta", 8000);
+        //
+        // Console.WriteLine(userData);
         
-        // Boolean
-        bool active = true;
+        // Dates
+       
+        DateOnly day = new DateOnly(2024, 3, 19);
+
+        string dayInText = day.ToString(new CultureInfo("en-US"));
+
+        DateTime today = DateTime.UtcNow;
+
+        DateTime tomorrow = today.AddDays(1);
         
-        // Char
-        char letter = 'A'; // single quotes
-        
-        // String
-        string name = "Vegeta"; // double quotes
-
-        char firstLetter = name[0];
-        
-        Console.WriteLine(firstLetter);
-        
-        Console.WriteLine(doubleNumber);
-        Console.WriteLine(doubleNumber.GetType().Name);
-
-        string path = @"C:\test"; // "C:\\test"
-
-        string firstName = "Iori";
-        string lastName = "Yagami";
-
-        string fullName = $"{firstName} {lastName}";
-        
-        // String Builder
-        StringBuilder stringBuilder = new StringBuilder();
-
-        stringBuilder.Append(firstName);
-        stringBuilder.Append(lastName);
-
-        string result = stringBuilder.ToString();
-
-        string user = "Name: {0}. \nPassword: {1}";
-
-        string userData = string.Format(user, "Vegeta", 8000);
-        
-        Console.WriteLine(userData);
+        Console.WriteLine(dayInText);
+        Console.WriteLine(today);
+        Console.WriteLine(tomorrow);
     }
 }
 
